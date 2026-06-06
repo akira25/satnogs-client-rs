6ef350394422edc71034f681be438046204e3027
Martin Hübner <martin.hubner@web.de>
2026-06-06 16:51:25 +0200

    feat: impl pre/post-scripts

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-pub mod observation;
 pub mod settings;
 // pub mod tasks;
-// pub mod scheduler;
 pub mod queue_types;
-// pub mod tmp;
+
+#[cfg(feature = "sdr_stream")]
+pub mod sdr;
 
 use crate::observation::Observation;
 use crate::queue_types::QueueJob;
@@ -14,7 +14,8 @@ use satnogs_apiclient::{
 	filters::JobFilter,
 	json::Job as ApiJob,
 };
-use std::collections::{BinaryHeap, VecDeque};
+use serde_json;
+use std::{collections::{BinaryHeap, VecDeque}, io::Write};
 use std::fs;
 use std::path::PathBuf;
 use std::process::{Child, Command};
@@ -74,9 +75,14 @@ pub fn task_observation_housekeeping(
 				let _ = child.kill();
 				let _ = child.wait();
 				info!("Stopped flowgraph: 'ToDo'");
+				// ToDo: This should be in a separate thread or in upload-task running in separate thread
+				let _ = run_obs_scripts(
+					conf.clone(),
+					ObservationScript::Post,
+					job.clone(),
+				);
 			}
 
-			// current job finished
 			*curr_job_info = None;
 		}
 	}
@@ -90,21 +96,27 @@ pub fn task_observation_housekeeping(
 	if let Some(next_job) = next_job_queue.peek()
 		&& next_job.start <= now
 	{
+		// ToDo: Das hier schick machen
 		let cmd = CmdArgs {
-			program: "python3".to_string(),
-			args: vec!["./test.py".to_string(), "-a".to_string()],
-			pwd: conf.storage.artifacts_path.into(),
+			program: "test.py".to_string(),
+			args: vec!["-a".to_string()],
+			pwd: format!("{}/{}/", conf.storage.artifacts_path, next_job.job.id).into(),
 		};
+		let cmd2 = cmd.clone();
+
+		let _ = run_obs_scripts(conf, ObservationScript::Pre, next_job.job.clone());
 
 		match Command::new(cmd.program).args(cmd.args).current_dir(cmd.pwd).spawn() {
 			Err(_) => {
-				error!("Failed to spawn flowgraph 'ToDO'!");
+				error!(
+					"Failed to spawn flowgraph {} {:?}",
+					cmd2.program, cmd2.args
+				);
 			},
 
 			Ok(child) => {
-				info!("Started flowgraph: 'ToDo'");
+				info!("Started flowgraph: {} {:?}", cmd2.program, cmd2.args);
 
-				// save process handle
 				*curr_job_handle = Some(child);
 
 				// move job from queue -> current_job
@@ -114,19 +126,69 @@ pub fn task_observation_housekeeping(
 	}
 }
 
-/// Changes to dir /tmp/satnogs-rs/$observation_id/ and executes post-scripts in lexicographical order
-fn run_post_scripts(conf: Settings) {
-	if conf.scripting.post_observation_script {
-		let scripts = fs::read_dir(conf.scripting.post_obs_path).unwrap();
-		for script in scripts {
-			todo!();
+enum ObservationScript {
+	Pre,
+	Post,
+}
+
+/// Run Pre/Post Observation scripts in lexicographical order. For that, we
+/// change directory to /tmp/satnogs-rs/$observation_id/
+fn run_obs_scripts(conf: Settings, pre_post: ObservationScript, job: ApiJob) -> anyhow::Result<()> {
+	let (enabled, script_path, script_type) = match pre_post {
+		ObservationScript::Pre => {
+			(conf.scripting.pre_observation_script, &conf.scripting.pre_obs_path, "pre")
+		},
+		ObservationScript::Post => (
+			conf.scripting.post_observation_script,
+			&conf.scripting.post_obs_path,
+			"post",
+		),
+	};
+
+	if !enabled {
+		return Ok(());
+	}
+
+	debug!("Creating observation '{}' directory.", job.id);
+	let cwd = format!("{}/{}", conf.storage.artifacts_path, job.id);
+	fs::create_dir_all(&cwd)?;
+
+	debug!("Saving job information as json...");
+	let job_json_path = format!("{}/{}", cwd, "job.json");
+	let mut f = fs::File::create(job_json_path)?;
+	let json = serde_json::to_string(&job)?;
+	match f.write(&json.into_bytes()) {
+		Ok(bytes) => debug!("Written {bytes} bytes."),
+		Err(e) => error!("Writing job json failed: {}", e),
+	}
+
+
+	let mut entries: Vec<_> = fs::read_dir(script_path)?.filter_map(Result::ok).collect();
+	entries.sort_by_key(|e| e.path());
+
+	for entry in entries {
+		let path = entry.path();
+		if !path.is_file() {
+			continue;
+		}
+
+		match Command::new(&path).current_dir(&cwd).spawn() {
+			Ok(mut child) => {
+				info!("Running {}-script: '{:?}'", script_type, path);
+				child.wait()?;
+			},
+			Err(e) => {
+				error!("Running {}-script '{:?}' failed: {}", script_type, path, e);
+				error!("Did you set the exec-bit for the script?")
+			},
 		}
 	}
+
+	Ok(())
 }
 
 pub fn task_upload_artifacts(upload_queue: Arc<Mutex<BinaryHeap<QueueJob>>>) -> anyhow::Result<()> {
 	debug!("Checking for artifacts to be uploaded...");
-	debug!("Nothing to be uploaded.");
 
 	let mut upload_queue = upload_queue.lock().unwrap();
 	debug!("Acquired upload_queue mutex.");
@@ -151,7 +213,10 @@ pub fn task_upload_artifacts(upload_queue: Arc<Mutex<BinaryHeap<QueueJob>>>) -> 
 			// Upload was successful
 			let _ = upload_queue.pop();
 		},
-		None => return Ok(()),
+		None => {
+			debug!("Nothing to be uploaded.");
+			return Ok(());
+		},
 	};
 
 	Ok(())
@@ -196,25 +261,44 @@ pub fn fetch_jobs(api: APIClient, conf: Settings) -> anyhow::Result<Vec<ApiJob>>
 	Ok(api.get_jobs_heartbeat(f, station_info, conf.network.token)?)
 }
 
+#[derive(Debug, Clone)]
 pub struct CmdArgs {
 	pub program: String,
 	pub args: Vec<String>,
 	pub pwd: PathBuf,
 }
 
-fn flowgraph_task() {
-	let script = "./test.py";
-	let handle = Command::new("python3").arg(script).spawn();
+mod test {
+	use chrono::{DateTime, Utc};
 
-	match handle {
-		Err(_) => {
-			error!("Failed to spawn flowgraph '{script}'!")
-		},
-		Ok(h) => {
-			info!("Started flowgraph: '{script}'");
-			println!("{:?}", h);
-		},
+	use super::*;
+
+	#[test]
+	fn pre_post_scripts() {
+		let conf = Settings::new().unwrap();
+		let job = ApiJob {
+			id: 42,
+			start: DateTime::parse_from_rfc3339("2026-01-31T00:00:00Z")
+				.unwrap()
+				.with_timezone(&Utc),
+			end: DateTime::parse_from_rfc3339("2026-01-31T00:02:00Z")
+				.unwrap()
+				.with_timezone(&Utc),
+			ground_station: 42,
+			tle0: "".to_string(),
+			tle1: "".to_string(),
+			tle2: "".to_string(),
+			frequency: 430200000,
+			mode: Some("GMSK".to_string()),
+			transmitter: "abf4329bdee".to_string(),
+			baud: Some(9600.0),
+			max_altitude: 80.0,
+			norad_cat_id: 42000,
+		};
+		let _ = run_obs_scripts(conf.clone(), ObservationScript::Pre, job.clone());
+		let _ = run_obs_scripts(conf, ObservationScript::Post, job);
+
+		// assert job json
+		// assert
 	}
-
-	// return handle;
 }
